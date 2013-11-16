6d4c4eedf75d5f36b57d671600a20423542475dd
Z3n85 <>
2013-11-15 21:37:39 -0800

    added notification via e-mail

--- createblacklist.sh
+++ createblacklist.sh
@@ -2,9 +2,13 @@
 #  Creates DNS blacklist for unbound server
 ##################################################################################
 #Config bits
-
+#Notifification email
+EMAIL="changeme@changeme.com"
 #Final blacklistlist location
 FINALLIST="/var/unbound/etc/blacklist.list"
+
+#static file option
+STATICBLACKLIST="/usr/local/dnsblacklist/static.list"
 
 #URL to get list of ad servers to blacklist from
 BLACKLISTURL="http://pgl.yoyo.org/as/serverlist.php?hostformat=adblock;showintro=0"
@@ -23,20 +27,20 @@ UNBOUNDRESTART=/etc/rc.d/unbound\ restart
 ###################################################################################
 #This part does stuff
 
-#Housecleaning old file, just in case
-rm $FINALLIST
 
-#Get blacklist, Parse out non-URL entries
-curl $BLACKLISTURL | sed -n '/[^a-z0-9\.\*]/!p' | sed -n '/[a-zA-Z0-0]/p' |while read line; do
+blacklist="$(curl $BLACKLISTURL)"
+static="$(cat $STATICBLACKLIST)"
 
-	echo "local-zone: \"$line\" redirect" >> $FINALLIST
-	echo "local-data: \"$line A 127.0.0.1\"" >> $FINALLIST
+#Get blacklist, Parse out non-URL entries, dump to blacklist file
+output=$(echo "$blacklist$static" | sed -n '/[^a-z0-9\.\*]/!p' | sed -n '/[a-zA-Z0-0]/p' | awk '{print "local-zone: \""$0"\" redirect\nlocal-data: \""$0" A 127.0.0.1\""}') 
+echo "$output" > $FINALLIST
 
-done
-
+#cat $STATICBLACKLIST | sed -n '/[^a-z0-9\.\*]/!p' | sed -n '/[a-zA-Z0-0]/p' | awk '{print "local-zone: \""$0"\" redirect\nlocal-data: \""$0" A 127.0.0.1\""}' >> $FINALLIST
 #change permissions on final blacklist file
 chown $OWNER:$GROUP $FINALLIST
 chmod $PERMISSIONS $FINALLIST
 
 #restart service
-$UNBOUNDRESTART
+#output="$(echo "$output"; $UNBOUNDRESTART)"
+#echo "$output"
+$UNBOUNDRESTART | mail -s "YourDNSBlockUpdate" $EMAIL 
